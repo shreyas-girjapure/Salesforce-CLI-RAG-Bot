bf9ccdf16e6c849c299fba61939590d5e2e4faad
Shreyas Girjapure <66116603+shreyas-girjapure@users.noreply.github.com>
2023-10-30 11:25:07 +0000

    finalizing

--- script.js
+++ script.js
@@ -1,41 +1,69 @@
 const queryInput = document.getElementById('query');
+const darkModeSwitch = document.getElementById('dark-mode-switch');
+const darkModeIcon = document.getElementById('dark-icon');
 const responseContainer = document.getElementById('responseContainer');
 const vectorSearchButton = document.getElementById('vector-search');
 const llmSearchButton = document.getElementById('llm-search');
+const errorTextElement = document.getElementById('errorText');
+const body = document.querySelector('body');
+
+
 const baseUrl = 'https://basicauth-znr4.onrender.com';
 let searchMode = 'search';//default
 let isRequestSuccess = false;
 
 
-async function performSearch(e) {
+async function performSearch(event) {
     try {
-        let buttonId = e?.target?.id;        
-        if(buttonId === 'llm-search'){
+        let buttonId = event?.target?.id;
+        if (buttonId === 'llm-search') {
             searchMode = 'super-search'
         }
         const query = queryInput.value;
+        responseContainer.classList.add('slds-hide')
+
         const finalURL = `${baseUrl}/${searchMode}?query=${encodeURIComponent(query)}`;
         console.log('here s url ' + finalURL);
 
         let primaryResponse = await fetch(finalURL);
-        if(primaryResponse.status == '200'){
+        let textData = await primaryResponse.text();
+        if (primaryResponse.status == '200') {
             isRequestSuccess = true;
-            let textData = await primaryResponse.text();
-            console.log(textData);
-            console.log(responseContainer)
-            responseContainer.innerHTML = textData;
+        } else {
+            isRequestSuccess = false;
         }
+        responseContainer.classList.remove('slds-hide');
+        responseContainer.innerHTML = textData;
 
     } catch (error) {
+        responseContainer.classList.remove('slds-hide');
+        responseContainer.innerHTML = `<pre>${error}</pre>`;
         console.log(error)
     }
 }
 
 vectorSearchButton.addEventListener('click', performSearch);
-llmSearchButton.addEventListener('click', performSearch);
+llmSearchButton?.addEventListener('click', performSearch);
+
+darkModeSwitch.addEventListener('click', handleDarkModeSwitch);
 
 queryInput.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
         performSearch();
     }
 });
+
+function handleDarkModeSwitch(event) {
+    console.log('dark mode toggle start');
+    toogleModes();
+    if(darkModeIcon.classList.contains('fa-sun')){
+        darkModeIcon.classList.replace('fa-sun','fa-moon')
+    }else{
+        darkModeIcon.classList.replace('fa-moon','fa-sun')
+    }
+}
+
+function toogleModes() {
+    body.classList.toggle('darkModeRoot');
+    responseContainer.classList.toggle('containerDark');
+}
